b7138a8e5dfab3270339a7358f2059573de60093
amj133 <amj@vt.edu>
2018-02-07 15:36:18 -0700

    relationships for course and student model tests passing

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,6 @@ class Student < ApplicationRecord
   validates :name, presence: true
 
   has_many :addresses, dependent: :delete_all
+  has_many :student_courses
   has_many :courses, through: :student_courses
 end
