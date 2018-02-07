92ea889bdc0deeb5b41764497719a1edc50019dc
amj133 <amj@vt.edu>
2018-02-07 15:40:01 -0700

    updated user can see individual student feature test to include student courses

--- spec/features/students/user_can_see_individual_student_spec.rb
+++ spec/features/students/user_can_see_individual_student_spec.rb
@@ -4,10 +4,15 @@ describe "user can see individual student" do
   context "user visits student show page" do
     it "displays individual student info" do
       student = Student.create!(name: "Bob Jones")
+      student.courses.create!(name: "Biology")
+      student.courses.create!(name: "Physics")
 
       visit student_path(student)
 
       expect(page).to have_content("Student Name: Bob Jones")
+      expect(page).to have_content("Courses:")
+      expect(page).to have_content("Biology")
+      expect(page).to have_content("Physics")
     end
   end
 end
