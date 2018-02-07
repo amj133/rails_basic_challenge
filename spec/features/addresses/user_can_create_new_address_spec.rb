6f7ca8c3fc7e9dd349aa6cce6d4ec3fc314f4a01
amj133 <amj@vt.edu>
2018-02-07 14:39:49 -0700

    write create new address feature test

---
+++ spec/features/addresses/user_can_create_new_address_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "user can create a new address for a student" do
+  context "visits address new page" do
+    it "can create an address" do
+      student = Student.create!(name: "Bill Bob")
+
+      visit new_student_address(student)
+      fill_in('address[description]', with: "Summer Home")
+      fill_in('address[street]', with: "123 Fake St.")
+      fill_in('address[street]', with: "Richmond")
+      fill_in('address[street]', with: "VA")
+      fill_in('address[street]', with: "55113")
+      click_on('Create New Address')
+
+      expect(current_path).to eq(student_path(student))
+      expect(page).to have_content("Description: Summer Home")
+      expect(page).to have_content("123 Fake St.")
+      expect(page).to have_content("Richmond, VA, 55113")
+    end
+  end
+end
