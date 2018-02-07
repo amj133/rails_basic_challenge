761d0709192f4392fa124184225ab067ff996729
amj133 <amj@vt.edu>
2018-02-07 15:51:49 -0700

    user can see course and list of students feature test passing

--- spec/features/courses/user_can_see_course_with_students_spec.rb
+++ spec/features/courses/user_can_see_course_with_students_spec.rb
@@ -4,7 +4,7 @@ describe "user can see course and list of students" do
   context "visits course show page" do
     it "displays course name and list of students" do
       course = Course.create!(name: "Physics")
-      course.students.create!(name: "Bill Bob")
+      course.students.create!(name: "Billy Bob")
       course.students.create!(name: "Sally Sue")
 
       visit course_path(course)
